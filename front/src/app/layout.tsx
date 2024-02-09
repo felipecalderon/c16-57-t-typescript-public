ee71638aba3189fcea66c88b9c93a1ca8ea9af94
Felipe <felipe.calderon321@gmail.com>
2024-02-09 18:29:26 -0300

    base del front ready

---
+++ front/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "App Juntadas (nombre por definir)",
+  description: "Para juntarse y pasarlo bien",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className}`}>{children}</body>
+    </html>
+  );
+}
