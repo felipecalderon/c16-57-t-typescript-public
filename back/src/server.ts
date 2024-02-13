51b8eb0ab8c11351296c7eefb65f7281722d362f
Victor Ramirez <vicdeveloper83@gmail.com>
2024-02-13 15:55:46 +0000

    server updated with mongo connection

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,6 +1,15 @@
 import app from "./app";
 import { envs } from "./config/plugins/envs/envs.plugin";
 
+import { MongoDatabase } from "./data/mongo";
+
+(async () => {
+  await MongoDatabase.connect({
+    mongoUrl: envs.MONGO_URL,
+    dbName: envs.MONGO_NAME,
+  });
+})();
+
 app.listen(envs.PORT, () => {
   console.log(`Server running on port ${envs.PORT}`);
 });
