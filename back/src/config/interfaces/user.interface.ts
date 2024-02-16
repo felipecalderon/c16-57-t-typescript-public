4d980329949c69b4a419f022db8cbfad0edbd643
Felipe <felipe.calderon321@gmail.com>
2024-02-15 23:04:43 -0300

    optional interest events expenses

--- back/src/config/interfaces/user.interface.ts
+++ back/src/config/interfaces/user.interface.ts
@@ -2,12 +2,12 @@ import { Document, Schema } from "mongoose";
 
 export interface IUser extends Document {
     name: string;
-    age: string;
+    age: Number;
     email: string;
     password: string;
     signUpDate: Date;
     location: string;
-    interestIds: string[];
-    events: Schema.Types.ObjectId[];
-    expenses: Schema.Types.ObjectId[];
+    interestIds?: string[];
+    events?: Schema.Types.ObjectId[];
+    expenses?: Schema.Types.ObjectId[];
   }
