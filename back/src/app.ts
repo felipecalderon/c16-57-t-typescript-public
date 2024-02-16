c2583997351cb4a4f24887a31728c0b1fc3caade
Felipe <felipe.calderon321@gmail.com>
2024-02-15 23:04:13 -0300

    indexed api global routes

--- back/src/app.ts
+++ back/src/app.ts
@@ -3,18 +3,19 @@ import cors from "cors";
 import morgan from "morgan";
 
 import { envs } from "./config/plugins/envs/envs.plugin";
-
+import routes from './routes/index'
 const app = express();
 //*Middlewares===============
 envs.NODE_ENV === "development" && app.use(morgan("dev"));
 app.use(cors({ origin: "*" }));
 
-app.use(express.json());
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 //*Middlewares===============
 
 //?Routes====================
-
+app.use('/api', routes)
 //?Routes====================
 
 export default app;
