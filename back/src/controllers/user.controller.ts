cede92d7a8e7ecf93b95b93dbf2606a1fb55afad
Felipe <felipe.calderon321@gmail.com>
2024-02-16 23:29:33 -0300

    upgrade controller functions

--- back/src/controllers/user.controller.ts
+++ back/src/controllers/user.controller.ts
@@ -1,42 +1,52 @@
 import { IUser } from "../config/interfaces/user.interface";
 import User from "../data/mongo/models/user.model";
 import bcrypt from "bcrypt";
+import {
+  createUserDB,
+  deleteUserDB,
+  getUsersDB,
+  updateUserDB,
+} from "../services/user.service";
+import { Request, Response } from "express";
 
-export const getUsers = async (id?: string) => {
+export const getUsersController = async (req: Request, res: Response) => {
   try {
     // Buscar usuario si existe id en req.query
-    if (id) {
-      const user = await User.findById(id).select("-password"); //Extrae password en la respuesta
-      if (!user) throw new Error("No se encontró el usuario");
+    const { userId } = req.query;
+    if (userId) {
+      const user = await getUsersDB(userId as string); // Obtiene usuarios del servicio
+      if (!user) res.status(404).json({ error: "Usuario no encontrado" });
+      else res.status(200).json(user);
       return user;
     }
 
     // Buscar todos los usuarios
     const users = await User.find().select("-password"); //Extrae password en la respuesta
-    return users;
+    return res.status(200).json(users);
   } catch (error) {
     console.error(error);
-    throw "Hubo un error al buscar usuarios";
+    res.status(500).json({ error: "Hubo un error al buscar usuarios" });
   }
 };
 
-export const createUser = async ({
-  name,
-  age,
-  email,
-  password,
-  location,
-  interestIds,
-  events,
-  expenses,
-}: IUser) => {
+export const createUserController = async (req: Request, res: Response) => {
   try {
+    const {
+      name,
+      age,
+      email,
+      password,
+      location,
+      interestIds,
+      events,
+      expenses,
+    } = req.body;
     // Hashear la contraseña antes de guardar el usuario
     const saltRounds = 10; // Cantidad de saltos para encriptar el hash
     const hashedPassword = await bcrypt.hash(password, saltRounds);
 
-    // Crear instancia de usuario
-    const newUser = new User({
+    // Guardar usuario en DB
+    const newUser = createUserDB({
       name,
       age,
       email,
@@ -45,44 +55,40 @@ export const createUser = async ({
       interestIds,
       events,
       expenses,
-    });
-
+    } as IUser);
     // PENDIENTE: validar datos antes de crear usuario
-    // Guardar usuario en DB
-    await newUser.save();
-    return "Usuario creado exitosamente";
+    res.status(200).json(newUser);
   } catch (error) {
     console.error(error);
-    throw "Hubo un error al crear el usuario";
+    res.status(500).json({ error: "Hubo un error al crear el usuario" });
   }
 };
 
-export const updateUser = async (id: string, updateData: Partial<IUser>) => {
+export const updateUserController = async (req: Request, res: Response) => {
   try {
     // PENDIENTE: validar datos antes de actualizar (middleware ¿?)
-    const user = await User.findByIdAndUpdate(id, updateData, {
-      new: true,
-    }).select("-password");
-    if (!user) {
-      throw "No se encontró el usuario";
-    }
-    return user;
+    const { userId } = req.query;
+    const updatedUser = await updateUserDB(userId as string, req.body);
+    if (!updatedUser)
+      return res.status(404).json({ error: "Usuario no encontrado" });
+    else return res.status(200).json(updatedUser);
   } catch (error) {
     console.error(error);
-    throw "Error al actualizar el usuario";
+    res.status(500).json({ error: "Hubo un error al actualizar usuario" });
   }
 };
 
-export const deleteUser = async (id: string) => {
+export const deleteUserController = async (req: Request, res: Response) => {
   try {
-    const user = await User.findByIdAndDelete(id);
-    if (!user) {
-      throw "No se encontró el usuario";
+    const { userId } = req.query;
+    const deletedUser = await deleteUserDB(userId as string);
+    if (!deletedUser) {
+      return res.status(404).json({ error: "Usuario no encontrado" });
     }
-    return "Usuario eliminado exitosamente";
+    return res.status(200).json(deletedUser);
   } catch (error) {
     console.error(error);
-    throw "Error al eliminar el usuario";
+    res.status(500).json({ error: "Hubo un error al actualizar usuario" });
   }
 };
 
