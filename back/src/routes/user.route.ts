b7fb9d9dfcffd2aade86e4cc9fe6b51eb8b45d54
Felipe <felipe.calderon321@gmail.com>
2024-02-17 01:00:35 -0300

    simulate protected route (delete user)

--- back/src/routes/user.route.ts
+++ back/src/routes/user.route.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { createUserController, deleteUserController, getUsersController, updateUserController } from "../controllers/user.controller";
+import { verifyToken } from "../middlewares/verifyToken";
 
 const route = Router();
 
 route.get("/", getUsersController);
 route.post("/", createUserController);
 route.put("/", updateUserController);
-route.delete("/", deleteUserController);
+route.delete("/", verifyToken, deleteUserController);
 
 export default route;
