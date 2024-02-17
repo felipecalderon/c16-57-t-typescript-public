8d9c11c2bd68583c63b155d4421689dabb548fb2
Felipe <felipe.calderon321@gmail.com>
2024-02-17 01:00:12 -0300

    global auth routes

--- back/src/routes/index.ts
+++ back/src/routes/index.ts
@@ -1,8 +1,10 @@
 import userRoute from './user.route'
+import authRoute from './auth.route'
 import { Router } from 'express'
 
 const route = Router()
 
 route.use('/users', userRoute)
+route.use('/auth', authRoute)
 
 export default route
