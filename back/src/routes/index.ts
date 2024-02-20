639ce85d5e00d23f5792978ecd28e62886928213
Felipe <felipe.calderon321@gmail.com>
2024-02-20 00:52:00 -0300

    feat: new route added events

--- back/src/routes/index.ts
+++ back/src/routes/index.ts
@@ -1,10 +1,12 @@
 import userRoute from './user.route'
 import authRoute from './auth.route'
+import eventsRoute from './events.route'
 import { Router } from 'express'
 
 const route = Router()
 
 route.use('/users', userRoute)
 route.use('/auth', authRoute)
+route.use('/events', eventsRoute)
 
 export default route
