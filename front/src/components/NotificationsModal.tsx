95702d1d093441be71354716ccee482e2068dba3
Adrian4891 <ernebarra36@gmail.com>
2024-02-29 17:13:53 -0300

    add notifications format

--- front/src/components/NotificationsModal.tsx
+++ front/src/components/NotificationsModal.tsx
@@ -15,22 +15,37 @@ import { PiConfettiDuotone } from "react-icons/pi";
 const NotificationsModal = () => {
   return (
     <Dialog>
-      <DialogTrigger><IoIosNotifications/></DialogTrigger>
+      <DialogTrigger>
+        <IoIosNotifications />
+      </DialogTrigger>
       <DialogContent>
         <DialogHeader>
           <DialogTitle className="font-bold pb-2">Notificaciones</DialogTitle>
           <DialogDescription>
             <ul className="flex flex-col gap-3">
-                <li className="font-semibold text-md"><IoPersonCircleSharp className="inline text-lg"/> Santi te agrego al evento "Juntada en la casa de Santi"</li>
-                <li className="font-semibold text-md"><IoPersonCircleSharp className="inline text-lg"/> Eric modifico el evento "Encuentro en Rabia Bar"</li>
-                <li className="font-semibold text-md"><GiPayMoney className="inline text-lg"/> Juan completo el ultimo gasto restante. No tienes saldos restantes <PiConfettiDuotone className="inline text-lg"/></li>
-                <li className="font-semibold text-md"><FaRegCheckCircle className="inline text-lg"/> Caro confirmo su asistencia a tu evento</li>
+              <li className="font-semibold text-md">
+                <IoPersonCircleSharp className="inline text-lg" /> Santi te
+                agrego al evento "Juntada en la casa de Santi"
+              </li>
+              <li className="font-semibold text-md">
+                <IoPersonCircleSharp className="inline text-lg" /> Eric modifico
+                el evento "Encuentro en Rabia Bar"
+              </li>
+              <li className="font-semibold text-md">
+                <GiPayMoney className="inline text-lg" /> Juan completo el
+                ultimo gasto restante. No tienes saldos restantes{" "}
+                <PiConfettiDuotone className="inline text-lg" />
+              </li>
+              <li className="font-semibold text-md">
+                <FaRegCheckCircle className="inline text-lg" /> Caro confirmo su
+                asistencia a tu evento
+              </li>
             </ul>
           </DialogDescription>
         </DialogHeader>
       </DialogContent>
     </Dialog>
-   );
+  );
 };
 
 export default NotificationsModal;
