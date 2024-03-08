c8ab279788878e202fe7624b12a1f25a870e7e2b
Adrián Nicolás Onores <nicolasonores@hotmail.com>
2024-03-08 16:05:51 -0300

    add create button footer fix

--- front/src/app/dashboard/page.tsx
+++ front/src/app/dashboard/page.tsx
@@ -8,6 +8,9 @@ import axios from 'axios'
 import React, { useEffect } from 'react'
 import Cta from '@/components/Cta'
 import ModalCrear from '@/components/modalCrear'
+import  {Button} from '@/components/ui/button'
+import  Link  from 'next/link'
+
 
 export default function DashboardPage() {
     const fetchEvents = async () => {
@@ -34,6 +37,12 @@ export default function DashboardPage() {
         <>
            
             <Cta/>
+            <div className='flex justify-center items-center text-center'>
+            <Link className="w-48 font-semibold rounded bg-[#1A7754] my-2 py-4  hover:bg-[#3e9976] shadow-md hover:shadow-gray-400" href="events/create">
+               + Crear
+            </Link>
+            </div>
+
             <UpcomingEvents />
             <Recommended />
             <Footer />
--- front/src/components/Footer.tsx
+++ front/src/components/Footer.tsx
@@ -2,7 +2,9 @@
 
 const Footer = () => {
   return(
-    <footer className="bg-gray-800 p-4">
+    <footer className="
+    bg-[#1A7754]
+     p-4">
       <div className="container mx-auto text-center font-bold text-white">
         Proyecto de c16-57-t-typescript 
       </div>
